a8e424373832b87f3184d98af9cfa0d1ff529ba1
Gabriel Senziani <senzianigabriel@hotmail.com>
2026-09-22 14:37:48 -0300

    feat: enable CORS and add participations route for frontend integration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 
 import "dotenv/config";
 
@@ -16,6 +17,8 @@ const app = express()
 
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/cadastro", cadastroRouter)
 app.use("/login", loginRouter)
